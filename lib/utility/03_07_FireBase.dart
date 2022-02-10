469e94e9c8a9cf68f087d8837a4842465b4e4cec
yusuke777777 <yusuke2370@gmail.com>
2022-02-11 02:02:35 +0900

    歩数合計機能追加

--- lib/utility/03_07_FireBase.dart
+++ lib/utility/03_07_FireBase.dart
@@ -6,69 +6,83 @@ import 'health.dart';
 
 class FirestoreMethod {
   static FirebaseFirestore _firestoreInstance = FirebaseFirestore.instance;
-  static final pedmeterRef = _firestoreInstance.collection(
-      'PEDOMETER_TBL');
+  static final pedmeterRef = _firestoreInstance.collection('PEDOMETER_TBL');
   static final Firebase_Auth.FirebaseAuth auth =
       Firebase_Auth.FirebaseAuth.instance;
-  static  String housing = "住まい";
-  static  int todaySteps = 0;
-  static  int sumSteps = 0;
-  static  int sumKm = 0;
+  static String housing = "住まい";
+  static int todaySteps = 0;
+  static int sumSteps = 0;
+  static int sumKm = 0;
 
   static Future<void> makePedmeter(String location) async {
+    DateTime now = DateTime.now();
+    DateTime midnight = DateTime(now.year, now.month, now.day);
+    await HelthInfo().fetchStepData(now,now,midnight);
     try {
       await pedmeterRef.doc(auth.currentUser!.uid).set({
         'USER_ID': auth.currentUser!.uid,
         'HOUSING': location,
-        'TODAY_STEPS':todaySteps,
-        'SUM_STEPS':sumSteps,
-        'SUM_KM':sumKm,
+        'TODAY_STEPS': HelthInfo.steps!.toInt(),
+        'SUM_STEPS': HelthInfo.steps!.toInt(),
+        'SUM_KM': sumKm,
+        'ZEN_STEPS_DATE': DateTime.now()
       });
-    }
-    catch (e) {
+    } catch (e) {
       print('ユーザー登録に失敗しました --- $e');
     }
   }
 
   static Future<Pedmeter> getPedmeter() async {
-    await HelthInfo().fetchStepData();
-    DocumentSnapshot _PedmeterDoc = await pedmeterRef
-        .doc('${auth.currentUser!.uid}')
-        .get();
+    DocumentSnapshot _PedmeterDoc =
+        await pedmeterRef.doc('${auth.currentUser!.uid}').get();
+    Timestamp zenStepDatewk = _PedmeterDoc.get('ZEN_STEPS_DATE');
+    DateTime zenStepDate = zenStepDatewk.toDate();
+    DateTime now = DateTime.now();
+    DateTime midnight = DateTime(now.year, now.month, now.day);
+    await HelthInfo().fetchStepData(zenStepDate,now,midnight);
+
     try {
       housing = _PedmeterDoc.get('HOUSING');
+      print(housing);
       todaySteps = HelthInfo.steps!.toInt();
+      print(todaySteps);
       int sumStepsWk = _PedmeterDoc.get('SUM_STEPS');
-      sumSteps = sumStepsWk + HelthInfo.steps!.toInt();
+      print(sumStepsWk);
+      sumSteps = sumStepsWk + HelthInfo.plusSteps!.toInt();
+      print(sumSteps);
       sumKm = _PedmeterDoc.get('SUM_KM');
-    }
-    catch (e) {
+      zenStepDate = now;
+      print(sumKm);
+      print(zenStepDate);
+
+    } catch (e) {
       print('歩数取得に失敗しました --- $e');
     }
     try {
       await pedmeterRef.doc(auth.currentUser!.uid).set({
         'USER_ID': auth.currentUser!.uid,
         'HOUSING': housing,
-        'TODAY_STEPS':todaySteps,
-        'SUM_STEPS':sumSteps,
-        'SUM_KM':sumKm,
+        'TODAY_STEPS': todaySteps,
+        'SUM_STEPS': sumSteps,
+        'SUM_KM': sumKm,
+        'ZEN_STEPS_DATE': zenStepDate
       });
-    }
-    catch (e) {
+    } catch (e) {
       print('ユーザー登録に失敗しました --- $e');
     }
     Pedmeter myPedmeter = Pedmeter(
-      HOUSING: _PedmeterDoc.get('HOUSING'),
-      TODAY_STEPS:_PedmeterDoc.get('TODAY_STEPS'),
-      SUM_STEPS:_PedmeterDoc.get('SUM_STEPS'),
-      SUM_KM:_PedmeterDoc.get('SUM_KM')
-    );
+        HOUSING: _PedmeterDoc.get('HOUSING'),
+        TODAY_STEPS: _PedmeterDoc.get('TODAY_STEPS'),
+        SUM_STEPS: _PedmeterDoc.get('SUM_STEPS'),
+        SUM_KM: _PedmeterDoc.get('SUM_KM'));
 
     return myPedmeter;
   }
+
   static Stream<QuerySnapshot> pedmeterSnapshot() {
-    return pedmeterRef.doc(auth.currentUser!.uid).collection('PEDOMETER_TBL').snapshots();
+    return pedmeterRef
+        .doc(auth.currentUser!.uid)
+        .collection('PEDOMETER_TBL')
+        .snapshots();
   }
-
-
 }
--- lib/utility/health.dart
+++ lib/utility/health.dart
@@ -9,6 +9,7 @@ class HelthInfo {
   List<HealthDataPoint> _healthDataList = [];
   HealthFactory health = HealthFactory();
   static int? steps = 0;
+  static int? plusSteps = 0;
   static String? meter;
   // define the types to get
   var types = [
@@ -21,40 +22,20 @@ class HelthInfo {
 
 
   /// Fetch steps from the health plugin and show them in the app.
-  Future fetchStepData() async {
+  Future fetchStepData(DateTime zenStepDate,DateTime now,DateTime midnight) async {
     // get steps for today (i.e., since midnight)
-    final now = DateTime.now();
-    final midnight = DateTime(now.year, now.month, now.day);
 
     bool requested = await health.requestAuthorization([HealthDataType.STEPS]);
 
     if (requested) {
       try {
         steps = await health.getTotalStepsInInterval(midnight, now);
+        print(steps);
+        plusSteps = await health.getTotalStepsInInterval(zenStepDate, now) ?? 0;
       } catch (error) {
         print("Caught exception in getTotalStepsInInterval: $error");
       }
 
-      print('Total number of steps: $steps');
-
-    } else {
-      print("Authorization not granted");
-    }
-  }
-
-  Future fetchMeterData() async {
-    // get steps for today (i.e., since midnight)
-    final now = DateTime.now();
-    final midnight = DateTime(now.year, now.month, now.day);
-
-    bool requested = await health.requestAuthorization([HealthDataType.DISTANCE_WALKING_RUNNING]);
-
-    if (requested) {
-      try {
-        meter = (await health.getHealthDataFromTypes(midnight,now,types)) as String?;
-      } catch (error) {
-        print("Caught exception in getTotalStepsInInterval: $error");
-      }
       print('Total number of steps: $steps');
 
     } else {
