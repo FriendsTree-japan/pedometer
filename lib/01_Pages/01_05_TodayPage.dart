eeb2c3c7c13838052f07af73272f92d528ca6c44
RikitoKarita <rktkrt12@gmail.com>
2022-02-25 00:27:32 +0900

    前回指摘内容の取り込み

--- lib/01_Pages/01_05_TodayPage.dart
+++ lib/01_Pages/01_05_TodayPage.dart
@@ -22,6 +22,7 @@ class _TodayPage extends State<TodayPage> {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
+            title: Text('歩いて日本を1周しよう',style: TextStyle(color: Colors.black),),
             actions: [
               IconButton(
                 icon: Icon(Icons.navigate_next_outlined),
@@ -70,94 +71,274 @@ class _TodayPage extends State<TodayPage> {
             ),
           ),
           body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                      color: Colors.green,
-                      width: 10
-                  ),
-                ),
-                child: Image.asset("images/MtFuji.jpg"),
-              ),
-              Padding(padding: EdgeInsets.all(20)),
-              StreamBuilder<QuerySnapshot>(
-                stream: FirestoreMethod.pedmeterSnapshot(),
-                builder: (context, snapshot) {
-                  return FutureBuilder<Object>(
-                    future: FirestoreMethod.getPedmeter(),
-                    builder: (context, snapshot) {
-                      return Card(
-                        margin: const EdgeInsets.all(10.0),
-                        //color: Colors.green,
-                        child: Container(
-                          margin: const EdgeInsets.all(10.0),
-                          width: 300,
-                          height: 100,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Container(
-                                child: Text('本日の合計歩数',
-                                  style: TextStyle(fontSize: 16,color: Colors.black),),
-                              ),
-                              Container(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      child: Text(FirestoreMethod.todaySteps.toString(),
-                                        style: TextStyle(fontSize: 28,color: Colors.green),),
-                                    ),
-                                    Container(
-                                      child: Text('歩',
-                                        style: TextStyle(fontSize: 12,color: Colors.green),),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
+                padding: EdgeInsets.fromLTRB(10,20,10,10),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: const Color(0xFFE5E5E3),
+                            width: 10
                         ),
-                      );
-                    }
-                  );
-                }
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            color: const Color(0xFFE5E5E3),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text('現在地：'),
+                                Text('東京都'),
+                              ],
+                            ),),
+                          Container(child: Image.asset("images/MtFuji.jpg")),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
               Card(
-                margin: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.all(5),
+                color: const Color(0xFFE5E5E3),
                 //color: Colors.green,
                 child: Container(
-                  margin: const EdgeInsets.all(10.0),
-                  width: 300,
+                  margin: const EdgeInsets.fromLTRB(10,10,10,10),
+                  width: 350,
                   height: 100,
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Container(
-                        child: Text('東京制覇まであと',
-                          style: TextStyle(fontSize: 16,color: Colors.black),),
+                        child: Text('日本1周まであと',
+                          style: TextStyle(fontSize: 14,color: Colors.black),),
                       ),
                       Stack(
                         children: [
-                              SizedBox(
-                                  child: LinearProgressIndicator(
-                                    minHeight: 30.0,
-                                    backgroundColor: Colors.black12,
-                                    value: 0.2,
-                                  )),
+                          SizedBox(
+                              child: LinearProgressIndicator(
+                                minHeight: 40.0,
+                                color: Colors.black,
+                                backgroundColor: Colors.white,
+                                value: 0.2,
+                              )),
                         ],
                       ),
                       Container(
-                        child: Text('5/47都道府県制覇中',
-                          style: TextStyle(fontSize: 16,color: Colors.black),),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text('Next>',
+                              style: TextStyle(fontSize: 14,color: Colors.black),),
+                            Text('神奈川',
+                              style: TextStyle(fontSize: 14,color: Colors.black),),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
               ),
+              Padding(padding: EdgeInsets.fromLTRB(10, 10, 10, 10)),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            color: const Color(0xFFFFE1FF),
+                            width: 170,
+                            height: 90,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: Text('合計歩数',
+                                    style: TextStyle(fontSize: 14,color: Colors.black),),
+                                ),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        child: Text(FirestoreMethod.todaySteps.toString(),
+                                          style: TextStyle(fontSize: 20,color: Colors.pink),),
+                                      ),
+                                      Container(
+                                        child: Text('歩',
+                                          style: TextStyle(fontSize: 12,color: Colors.pink),),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.fromLTRB(10, 10, 10, 10)),
+                          Container(
+                            color: const Color(0xFFE9FFE1),
+                            width: 170,
+                            height: 90,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: Text('合計距離',
+                                    style: TextStyle(fontSize: 14,color: Colors.black),),
+                                ),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        child: Text(FirestoreMethod.todaySteps.toString(),
+                                          style: TextStyle(fontSize: 20,color: Colors.green),),
+                                      ),
+                                      Container(
+                                        child: Text('Km',
+                                          style: TextStyle(fontSize: 12,color: Colors.green),),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.fromLTRB(10, 5, 10, 5)),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            color: const Color(0xFEFFF3E1),
+                            width: 170,
+                            height: 90,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: Text('本日の歩数',
+                                    style: TextStyle(fontSize: 14,color: Colors.black),),
+                                ),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        child: Text(FirestoreMethod.todaySteps.toString(),
+                                          style: TextStyle(fontSize: 20,color: Colors.orange),),
+                                      ),
+                                      Container(
+                                        child: Text('歩',
+                                          style: TextStyle(fontSize: 12,color: Colors.orange),),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.fromLTRB(10, 10, 10, 10)),
+                          Container(
+                            color: const Color(0xFFE1ECFF),
+                            width: 170,
+                            height: 90,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: Text('本日の距離',
+                                    style: TextStyle(fontSize: 14,color: Colors.black),),
+                                ),
+                                Container(
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        child: Text(FirestoreMethod.todaySteps.toString(),
+                                          style: TextStyle(fontSize: 20,color: Colors.blue),),
+                                      ),
+                                      Container(
+                                        child: Text('Km',
+                                          style: TextStyle(fontSize: 12,color: Colors.blue),),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(20)),
+              // StreamBuilder<QuerySnapshot>(
+              //   stream: FirestoreMethod.pedmeterSnapshot(),
+              //   builder: (context, snapshot) {
+              //     return FutureBuilder<Object>(
+              //       future: FirestoreMethod.getPedmeter(),
+              //       builder: (context, snapshot) {
+              //         return Card(
+              //           margin: const EdgeInsets.all(10.0),
+              //           //color: Colors.green,
+              //           child: Container(
+              //             margin: const EdgeInsets.all(10.0),
+              //             width: 300,
+              //             height: 100,
+              //             child: Column(
+              //               mainAxisAlignment: MainAxisAlignment.start,
+              //               children: [
+              //                 Container(
+              //                   child: Text('本日の合計歩数',
+              //                     style: TextStyle(fontSize: 16,color: Colors.black),),
+              //                 ),
+              //                 Container(
+              //                   child: Row(
+              //                     mainAxisAlignment: MainAxisAlignment.center,
+              //                     children: [
+              //                       Container(
+              //                         child: Text(FirestoreMethod.todaySteps.toString(),
+              //                           style: TextStyle(fontSize: 28,color: Colors.green),),
+              //                       ),
+              //                       Container(
+              //                         child: Text('歩',
+              //                           style: TextStyle(fontSize: 12,color: Colors.green),),
+              //                       ),
+              //                     ],
+              //                   ),
+              //                 ),
+              //               ],
+              //             ),
+              //           ),
+              //         );
+              //       }
+              //     );
+              //   }
+              // ),
             ],
           ),),
     );
